eeb487684af14d1d54a09199023c5b5dd2614973
Taras Andrushko <tandrushko@romexsoft.com>
2019-01-11 16:24:23 +0200

    added server files

---
+++ controllers/image.js
@@ -0,0 +1,35 @@
+const Clarifai = require("clarifai");
+
+
+
+const app = new Clarifai.App({
+  apiKey: "d04d4904d6a24b1594413d9b4338a30e"
+});
+
+const handleApiCall = (req, res) => {
+  app.models.predict(Clarifai.FACE_DETECT_MODEL, req.body.input)
+    .then(data => {
+      res.json(data);
+    })
+    .catch(err => res.status(400).json('unable to work with API'))
+}
+
+
+
+const handleImage = (req, res, db) => {
+  const {
+    id
+  } = req.body;
+  db('users').where('id', '=', id)
+    .increment('entries', 1)
+    .returning('entries')
+    .then(entries => {
+      res.json(entries[0]);
+    })
+    .catch(err => res.status(400).json('unable to get entries'));
+}
+
+module.exports = {
+  handleImage: handleImage,
+  handleApiCall: handleApiCall
+};
